76384e87c4a423d26055bbb5c6244d96e6690074
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-06-08 15:13:46 -0300

    Erro padrao para ResponseStatusException

--- Clientes/src/main/java/br/com/github/kalilventura/Clientes/controller/ApplicationControllerAdvice.java
+++ Clientes/src/main/java/br/com/github/kalilventura/Clientes/controller/ApplicationControllerAdvice.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.github.kalilventura.clientes.controller.exception.ApiErrors;
 
@@ -28,6 +30,13 @@ public class ApplicationControllerAdvice {
                 .map(objectError -> objectError.getDefaultMessage()).collect(Collectors.toList());
 
         return new ApiErrors(messages);
+    }
 
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity handleResponseStatusException(ResponseStatusException exception) {
+        String errorMessage = exception.getMessage();
+        HttpStatus statusCode = exception.getStatus();
+        ApiErrors errors = new ApiErrors(errorMessage);
+        return new ResponseEntity<>(errors, statusCode);
     }
 }
