1b4259f5194e3f8571354d47183ceaa39356f55a
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-06-08 09:37:16 -0300

    Configuração de ambiente

--- src/main/java/io/github/com/kalilventura/VendasApplication.java
+++ src/main/java/io/github/com/kalilventura/VendasApplication.java
@@ -1,12 +1,19 @@
 package io.github.com.kalilventura;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VendasApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
+
+    /*
+     * Busca no arquivo application.properties a chave com o valor correspondente e
+     * injeta dentro da variavel
+     */
+    @Value("${spring.application.name}")
+    private String applicationName;
 }
