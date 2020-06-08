97c8b94ae56d71cbcad56bacc2a8dbaa6ed7d7b3
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-06-08 11:34:45 -0300

    Mapeando a entidade Cliente

--- Clientes/src/main/java/br/com/github/kalilventura/Clientes/ClientesApplication.java
+++ Clientes/src/main/java/br/com/github/kalilventura/Clientes/ClientesApplication.java
@@ -1,4 +1,4 @@
-package br.com.github.kalilventura.Clientes;
+package br.com.github.kalilventura.clientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
