ddab5493b436ecd80dfe22e031061f5be74f9b51
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-06-08 14:22:14 -0300

    Atualizando o cliente

--- Clientes/src/main/java/br/com/github/kalilventura/Clientes/controller/ClienteController.java
+++ Clientes/src/main/java/br/com/github/kalilventura/Clientes/controller/ClienteController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -44,6 +45,15 @@ public class ClienteController {
         repository.findById(id).map(cliente -> {
             repository.delete(cliente);
             return Void.TYPE;
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void alterarCliente(@PathVariable Integer id, @RequestBody Cliente clienteAtualizado) {
+        repository.findById(id).map(cliente -> {
+            clienteAtualizado.setId(cliente.getId());
+            return repository.save(clienteAtualizado);
         }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
