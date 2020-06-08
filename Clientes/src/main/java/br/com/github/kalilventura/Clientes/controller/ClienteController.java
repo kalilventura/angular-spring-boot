7575c1990d876269859a5dc56c7f93b425191c13
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-06-08 14:13:37 -0300

    Deletando o cliente

--- Clientes/src/main/java/br/com/github/kalilventura/Clientes/controller/ClienteController.java
+++ Clientes/src/main/java/br/com/github/kalilventura/Clientes/controller/ClienteController.java
@@ -2,6 +2,7 @@ package br.com.github.kalilventura.clientes.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,6 +36,15 @@ public class ClienteController {
     @GetMapping("{id}")
     public Cliente buscarPorId(@PathVariable Integer id) {
         return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Integer id) {
+        repository.findById(id).map(cliente -> {
+            repository.delete(cliente);
+            return Void.TYPE;
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
 }
