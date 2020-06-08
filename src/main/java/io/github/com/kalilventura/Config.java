b7f7de6d78cd99b41a302ea062ef23d59b960d0c
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-06-08 10:13:56 -0300

    Anotações customizadas

--- src/main/java/io/github/com/kalilventura/Config.java
+++ src/main/java/io/github/com/kalilventura/Config.java
@@ -1,18 +1,15 @@
 package io.github.com.kalilventura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("development")
+@Development
 public class Config {
 
-    /*
-     * Essa anotação permite que utilizemos em toda a aplicação
-     **/
-    @Bean(name = "applicationName")
-    public String applicationName() {
-        return "Sistema de Vendas";
+    @Bean
+    public CommandLineRunner execute() {
+        return args -> {
+            System.out.println("CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
     }
 }
