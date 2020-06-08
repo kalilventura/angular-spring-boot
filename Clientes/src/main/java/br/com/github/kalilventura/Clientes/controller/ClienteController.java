a64c5dd39efdebbe1e736d7d910393eaa736d1f4
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-06-08 14:08:38 -0300

    Buscar cliente por Id

--- Clientes/src/main/java/br/com/github/kalilventura/Clientes/controller/ClienteController.java
+++ Clientes/src/main/java/br/com/github/kalilventura/Clientes/controller/ClienteController.java
@@ -2,11 +2,14 @@ package br.com.github.kalilventura.clientes.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.github.kalilventura.clientes.model.entity.Cliente;
 import br.com.github.kalilventura.clientes.model.repository.ClienteRepository;
@@ -27,6 +30,11 @@ public class ClienteController {
     @ResponseStatus(code = HttpStatus.CREATED)
     public Cliente salvar(@RequestBody Cliente cliente) {
         return repository.save(cliente);
+    }
+
+    @GetMapping("{id}")
+    public Cliente buscarPorId(@PathVariable Integer id) {
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
 }
