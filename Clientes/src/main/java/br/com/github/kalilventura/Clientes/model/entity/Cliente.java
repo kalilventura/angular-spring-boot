dbde1d71947cc67fe24ad652955d588a86ac8ee9
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-06-08 11:54:35 -0300

    Adicionando o lombok no projeto

--- Clientes/src/main/java/br/com/github/kalilventura/Clientes/model/entity/Cliente.java
+++ Clientes/src/main/java/br/com/github/kalilventura/Clientes/model/entity/Cliente.java
@@ -8,16 +8,25 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
+/*
+    O lombok gera em tempo de compilação os Getters e Setters com as anotations @Getter e @Setter
+    Também podemos utilizar a @Data, onde ele cria o  ToString, @EqualsAndHashCode, @Getter, @Setter e
+    @RequiredArgsConstructor
+    Obs: Precisamos de um plugin na IDE para utilizar em tempo de desenvolvimento o lombok
+ */
 @Entity
+@Data
 public class Cliente {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    
+
     @Column(nullable = false, length = 150)
     private String nome;
-    
+
     @Column(nullable = false, length = 11)
     private String cpf;
 
