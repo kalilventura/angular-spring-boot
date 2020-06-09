5506933f1cff262c5c759352382672fe90d9d242
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-06-09 15:32:45 -0300

    Criacao pesquisa do servico prestado

--- Clientes/src/main/java/br/com/github/kalilventura/Clientes/controller/ServicoPrestadoController.java
+++ Clientes/src/main/java/br/com/github/kalilventura/Clientes/controller/ServicoPrestadoController.java
@@ -2,12 +2,15 @@ package br.com.github.kalilventura.clientes.controller;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -51,6 +54,12 @@ public class ServicoPrestadoController {
         servicoPrestado.setValor(bigDecimalConverter.converter(dto.getPreco()));
 
         return repository.save(servicoPrestado);
+    }
+
+    @GetMapping
+    public List<ServicoPrestado> pesquisar(@RequestParam(value = "nome", required = false) String nome,
+            @RequestParam(value = "mes", required = false) Integer mes) {
+        return repository.findClienteAndMes(nome, mes);
     }
 
 }
--- Clientes/src/main/java/br/com/github/kalilventura/Clientes/model/repository/ServicoRepository.java
+++ Clientes/src/main/java/br/com/github/kalilventura/Clientes/model/repository/ServicoRepository.java
@@ -1,9 +1,16 @@
 package br.com.github.kalilventura.clientes.model.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.github.kalilventura.clientes.model.entity.ServicoPrestado;
 
 public interface ServicoRepository extends JpaRepository<ServicoPrestado, Integer> {
 
+    @Query(" select s from ServicoPrestado s join s.cliente c "
+            + " where upper( c.nome ) like upper( :nome ) and MONTH(s.data) =:mes ")
+    List<ServicoPrestado> findClienteAndMes(@Param("nome") String nome, @Param("mes") Integer mes);
 }
