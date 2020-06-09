aa0fdecbf05b4a663ba97a18095282a473228a4a
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-06-09 16:57:29 -0300

    Validacao dados na API

--- Clientes/src/main/java/br/com/github/kalilventura/Clientes/controller/ServicoPrestadoController.java
+++ Clientes/src/main/java/br/com/github/kalilventura/Clientes/controller/ServicoPrestadoController.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -42,7 +44,7 @@ public class ServicoPrestadoController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ServicoPrestado salvar(@RequestBody ServicoPrestadoDTO dto) {
+    public ServicoPrestado salvar(@RequestBody @Valid ServicoPrestadoDTO dto) {
         LocalDate data = LocalDate.parse(dto.getData(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         Integer idCliente = dto.getIdCliente();
 
--- Clientes/src/main/java/br/com/github/kalilventura/Clientes/controller/dto/ServicoPrestadoDTO.java
+++ Clientes/src/main/java/br/com/github/kalilventura/Clientes/controller/dto/ServicoPrestadoDTO.java
@@ -1,13 +1,23 @@
 package br.com.github.kalilventura.clientes.controller.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class ServicoPrestadoDTO {
+    @NotEmpty(message = "{campo.descricao.obrigatorio}")
     private String descricao;
+
+    @NotEmpty(message = "{campo.preco.obrigatorio}")
     private String preco;
+
+    @NotEmpty(message = "{campo.data.obrigatorio}")
     private String data;
+
+    @NotNull(message = "{campo.cliente.obrigatorio}")
     private Integer idCliente;
 }
